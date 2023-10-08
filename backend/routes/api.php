0ed897b247cefd225eca58293cab1409879b6447
Angga Saputra <angsa0077@gmail.com>
2023-10-08 10:54:34 +0700

    berhasil register users backend

---
+++ backend/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\APIController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::namespace('App\Http\Controllers')->group(function () {
+    Route::post('/register', [APIController::class, 'register']);
+    Route::post('/login', [APIController::class, 'login']);
+    Route::post('/logout', [APIController::class, 'logout']);
+});
